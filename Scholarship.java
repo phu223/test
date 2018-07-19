17fa14e1964bcfe8fed5d3fb7f5c1497e83dfe90
phu223 <phupro348@gmail.com>
2018-07-19 13:57:28 +0700

    Add files via upload

---
+++ Scholarship.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Tachibana Kanade
+ */
+public class Scholarship {
+
+    /*camel case notation*/
+    public static int getAward(double gpa) {
+        if (gpa > 10 || gpa < 0) {
+            return -1;
+        } else if (gpa >= 9 && 10 >= gpa) {
+            return 1_000_000;
+        } else if (gpa >= 8 && gpa < 9) {
+            return 500_000;
+        } else {
+            return 0;
+        }
+    }
+    public static int getPassLab(double loc){
+         if (loc < 0) {
+            return -1;
+        } else if (loc >= 9 && 10 >= loc) {
+            return 1_000_000;
+        } else if (loc >= 8 && loc < 9) {
+            return 500_000;
+        } else {
+            return 0;
+        }
+    }
+}
